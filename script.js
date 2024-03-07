f6f1920ac8e72d7a80d0a6c1dd2c76e7ce2e6e11
Pat <hoang.patrick@outlook.com>
2024-03-07 16:30:21 +0100

    modification CSS

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const inputBox = document.getElementById("inputBox");
 const listContainer = document.getElementById("listContainer");
+const cardContainer = document.getElementById("cardContainer")
+const card = document.getElementById("card")
 
 function addTask() {
-  if (inputBox.value === "") {
-    alert("Merci d'entrer une tâche");
-  } else {
+
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
-  }
+  
   inputBox.value = "";
   saveData()
 }
@@ -22,6 +22,11 @@ listContainer.addEventListener("click", function (e) {
     saveData()
   }
 });
+
+function addCard() {
+  let newCard = card.cloneNode(true)
+  cardContainer.appendChild(newCard)
+}
 
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML );
